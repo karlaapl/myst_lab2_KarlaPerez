96f59accb624afdef3aea9f5b016d45afaf36a1b
Karla Perez <kp190496@gmail.com>
2018-10-01 08:48:08 -0500

    Lab hasta 01/10
    Ya está terminado  incluyendo la gráfica del activo y la cuenta.
    Se utiliza el ratio sharpe para ver el performance.

--- Admin_Activa.R
+++ Admin_Activa.R
@@ -112,6 +112,7 @@ Historico<-data.frame("Date" = row.names(Precios),
                       "R_Activo" = 0,
                       "R_Cuenta" = 0,
                       "Capital" = 0,
+                      "Flotante" = 0,
                       "Balance" = 0,"Titulos" = 0 ,"Titulos_a" = 0,
                       "Operacion" = NA, "Comisiones" = 0,"Comisiones_a"= 0, "Mensaje" = NA)
 
@@ -119,13 +120,14 @@ Historico<-data.frame("Date" = row.names(Precios),
 #Precio del activo 
 #R_Precio, rendimiento diario del precio(diario, de un dia a otro)
 #R_activo, rendimiento acumulado del precio 
-#Balance, valor del portafolio
+#Balance, valor del portafolio, capital mas flotante
 #titulos  ejecutados
 #titulos_a titulos acumulados
 #R_cuenta, balance+ capital 
 #operaciones, indica si son de compra o de venta(1 compra, 0 mantengo, -1 venta)
 #Comisiones 0.0025  por el valor de la transacción
 # Mensaje, Texto que indique la decisión o  que informe algo ocurrido
+# Flotante, el valor de los titulos acumulados por el precio del activo en el dia  pecio diario por titulos acumulados
 
 Regla0_R <- -0.015 # Considera una oportunidad de compra  cuando el activo empiece a bajar su rendimiento de -3% o menor 
 Regla1_I <- 0.2 # Porcentaje de capital que se utiliza para la operación inicial 
@@ -147,11 +149,12 @@ Historico$Titulos_a[1] <- Historico$Titulos[1]
 Historico$Comisiones[1] <- Historico$Titulos[1]*Historico$Precio[1]*Regla4_C
 Historico$Comisiones_a[1] <-Historico$Comisiones[1]
 
-# -- Calcular el Balance
-Historico$Balance[1] <- Historico$Titulos[1]*Historico$Precio[1]
+# -- Calcular el Balance y flotante
+#Historico$Balance[1] <-Historico$Titulos_a[1]*Historico$Precios[1] ya no por el cambio de flotante
+Historico$Balance[1] <- Historico$Flotante[1]*Historico$Capital[1]
 
 # -- Todo remanente se dejar? registrado en la cuenta de efectivo.
-Historico$Capital[1] <- Regla5_K-Historico$Balance[1]-Historico$Comisiones[1]
+Historico$Capital[1] <- Regla5_K-Historico$Flotante[1]-Historico$Comisiones[1]
 
 # -- Iniciamos con una postura de mantener.
 Historico$Operacion[1] <- "Posicion Inicial"
@@ -165,82 +168,123 @@ Historico$Mensaje[1] <- "Inicializacion de cartera"
 # -- Calcular R_Precio
 Historico$R_Precio <- round(c(0, diff(log(Historico$Precio))),4)
 
+# calcular r _activo 
+Postura_inicial <- Regla5_K%/% Historico$Precios[1]  # la diferencia entre  invertir todo el dinero desde el principio esperar a cuando se den señales de compre o venta
+
 # -- Calcular R_Activo
 for(i in 1:length(Historico$Date)){
-  Historico$R_Activo[i] <- round((Historico$Precio[i]/Historico$Precio[1])-1,2)
+  Historico$R_Activo[i] <-  (Postura_inicial*Historico$Precios[i])/(Postura_inicial*Historico$Precios[1])-1
 
 }
+
+
 # -- ------------------------------------ -- #
 # -- ------------------------------------ -- #
 # -- ------------------------------------ -- #
 
 for(i in 2:length(Historico$Date)){
+#for(i in 2:20){
 
-  if(Historico$R_Precio[i] <= Regla0_R){ 
-    # Establecer capital actual, inicialmente, igual al capital anterior
-    Historico$Capital[i] <- Historico$Capital[i-1]
-  }# Generar Señal
+  if(Historico$R_Precio[i] <= Regla0_R){  # si se cumple la regla y hay señal
 
-    if(Historico$Capital[i] > 0){ # Si hay capital
-
-      if(Historico$Capital[i]*Regla2_P > Historico$Precio[i]){ # Si Capital minimo
-
-        Historico$Operacion[i] <- "Compra"
-        Historico$Titulos[i]   <- (Historico$Capital[i]*Regla2_P)%/%Historico$Precio[i]
-
-        compra <- Historico$Precio[i]*Historico$Titulos[i]
-        Historico$Comisiones[i] <- compra*Regla4_C
-        Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]+Historico$Comisiones[i]
-
-        Historico$Titulos_a[i] <- Historico$Titulos_a[i-1]+Historico$Titulos[i]
-        Historico$Mensaje[i] <- "Compra Exitosa"
-        Historico$Balance[i] <- Historico$Precios[i]*Historico$Titulos_a[i]-Historico$Comisiones[i]
-      
-        
-        Historico$Capital[i] <- Historico$Capital[i-1]-compra-Historico$Comisiones[i]
-        Historico$R_Cuenta[i] <- Historico$Capital[i]+Historico$Balance[i]
-       
-
-      } # cierre si hay capital
-      else {
-        Historico$Mensaje[i] <- "Si hubo capital, pero no alcanzó "
-        
-      } #cierre no alcanzó el capital
-      
-
-    
-    else { # No hubo capital
-      
-
-    }
-
-
+      if(Historico$Capital[i-1] > 0){  # Si hay capital
+          Historico$Capital[i]<- Historico$Capital[i-1]
+        if(Historico$Capital[i]*Regla2_P > Historico$Precio[i]){ # Si Capital minimo
   
-  else { # Sin señal
-    Historico$Capital[i] <- Historico$Capital[i-1]
+            Historico$Operacion[i] <- 1
+            Historico$Titulos[i]   <- (Historico$Capital[i]*Regla2_P)%/%Historico$Precio[i]
+            compra <- Historico$Precio[i]*Historico$Titulos[i]
+            Historico$Comisiones[i] <- compra*Regla4_C
+            Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]+Historico$Comisiones[i]
+            Historico$Titulos_a[i] <- Historico$Titulos_a[i-1]+Historico$Titulos[i]
+            Historico$Mensaje[i] <- "Compra Exitosa"
+            Historico$Flotante[i] <- Historico$Precios[i]* Historico$Titulos_a[i]
+            Historico$Capital[i]<- Historico$Capital[i-1]- Historico$Titulos[i]*Historico$Precios[i]-Historico$Comisiones[i] #calcula el capital  despues del movimiento del dia i 
+            Historico$Balance[i] <- Historico$Capital[i]+ Historico$Flotante[i]
+            Historico$R_Cuenta[i]<- Historico$Balance[i]/Regla5_K -1
+        
+  
+        } # cierre si hay capital
+        else {
+          # no alcanzó el capital 
+          Historico$Mensaje[i] <- "Si hubo capital, pero no alcanzó "
+          Historico$Operacion[i] <- 0
+          Historico$Titulos[i] <- 0 
+          Historico$Titulos_a [i] <- Historico$Titulos_a[i-1]
+          Historico$Comisiones[i] <- 0
+          Historico$Comisiones_a[i]<- Historico$Comisiones_a[i-1]
+          Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precios[i]
+          Historico$Capital[i]<- Historico$Capital[i-1]- Historico$Titulos[i]*Historico$Precios[i]-Historico$Comisiones[i] #calcula el capital  despues del movimiento del dia i 
+          Historico$Balance[i] <- Historico$Capital[i]+ Historico$Flotante[i]
+          Historico$R_Cuenta[i]<- Historico$Balance[i]/Regla5_K -1
+          
+          
+        }} #cierre no alcanzó el capital
+        
+  
+      
+      else { # No hubo capital
+         Historico$Mensaje[i] <- "No hubo Capital "
+         Historico$Operacion[i] <- 0
+         Historico$Titulos[i] <- 0 
+         Historico$Titulos_a [i] <- Historico$Titulos_a[i-1]
+         Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precios[i]
+         Historico$Comisiones[i]<- 0
+         Historico$Comisiones_a[i] <-  Historico$Comisiones_a[i-1]
+         Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precios[i]
+         Historico$Capital[i]<- Historico$Capital[i-1]- Historico$Titulos[i]*Historico$Precios[i]-Historico$Comisiones[i] #calcula el capital  despues del movimiento del dia i 
+         Historico$Balance[i] <- Historico$Capital[i]+ Historico$Flotante[i]
+         Historico$R_Cuenta[i]<- Historico$Balance[i]/Regla5_K -1
+         
+      }}
+  
+  
     
-    Historico$Titulos[i] <- 0 # no hubo movimiento
-    Historico$Titulos_a[i] <- Historico$Titulos[i]+Historico$Titulos_a[i-1]
-    Historico$Balance[i] <- Historico$Titulos_a[i]*Historico$Precios[i]
-    Historico$R_Cuenta[i] <- Historico$Capital[i]+Historico$Balance[i]
-    Historico$Operacion[i] <- "Mantiene"
-    Historico$Comisiones[i] <- 0
-    Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]+Historico$Comisiones[i]
-    Historico$Mensaje[i] <- "No hubo señal de c "
-    Historico
-    
-    
-  }
+    else { # Sin señal
 
+      Historico$Titulos[i] <- 0 # no hubo movimiento
+      Historico$Titulos_a[i] <- Historico$Titulos[i]+Historico$Titulos_a[i-1]
+      Historico$Operacion[i] <- 0  #mantiene
+      Historico$Comisiones[i] <- 0
+      Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]+Historico$Comisiones[i]
+      Historico$Mensaje[i] <- "No hubo señal de compra "
+      Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precios[i]
+      Historico$Capital[i]<- Historico$Capital[i-1]- Historico$Titulos[i]*Historico$Precios[i]-Historico$Comisiones[i] #calcula el capital  despues del movimiento del dia i 
+      Historico$Balance[i] <- Historico$Capital[i]+ Historico$Flotante[i]
+      Historico$R_Cuenta[i]<- Historico$Balance[i]/Regla5_K -1
+      
+      
+      
+    } #cierre no hubo señal
+  
+    
+  
+  
+  
   
 
+    } #cierre for
 
+plot_ly(Historico) %>% # signo para agregar   las capas que se quieran 
+  
+  
+  add_trace(x = ~Date, y = ~round(R_Activo,4), type = 'scatter', mode = 'lines', name = 'Activo',
+            
+            line = list(color = 'red')) %>%
+  
+  add_trace(x = ~Date, y = ~round(R_Cuenta,4), type = 'scatter', mode = 'lines', name = 'Cuenta',
+            
+            line = list(color = 'blue')) %>% 
+  
+  layout(title = "Rend del activo VS Rend de la cuenta",
+         
+         xaxis = list(title = "Fechas", showgrid = T),
+         
+         yaxis = list(title = "Rendimiento"), 
+         
+         legend = list(orientation = 'h', y = -0.25, x = 0.5))
 
-
-
-    }
-  }
-
-
-
+###---- Metrico efectividad
+  SharpeRatio(R=xts(x = Historico$R_Activo,order.by = as.Date(Historico$Date)),Rf =0.0225, FUN ="StdDev" )
+  SharpeRatio(R=xts(x = Historico$R_Cuenta,order.by = as.Date(Historico$Date)),Rf =0.0225, FUN ="StdDev" )
 
